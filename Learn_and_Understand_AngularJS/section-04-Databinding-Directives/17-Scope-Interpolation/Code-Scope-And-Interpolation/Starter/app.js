584a5256eb9a4f1a350f20eef221bd379c76a5b5
Siddharth Mishra <siddharth.mishra@motorolasolutions.com>
2016-10-18 13:54:27 -0600

    Commenting.

--- Learn_and_Understand_AngularJS/section-04-Databinding-Directives/17-Scope-Interpolation/Code-Scope-And-Interpolation/Starter/app.js
+++ Learn_and_Understand_AngularJS/section-04-Databinding-Directives/17-Scope-Interpolation/Code-Scope-And-Interpolation/Starter/app.js
@@ -4,6 +4,7 @@ myApp.controller('mainController', ['$scope', '$timeout', function($scope, $time
     $scope.name = 'Siddharth';
     $scope.address = '7067 S Santa Cruz Dr., Apt RS22, Cottonwood Heights, UT-84121';
 
+    // Automatically the name would be changed and would be shown on page.
     $timeout(function(){
         $scope.name = 'Priyanka Pandey';
     }, 3000);
