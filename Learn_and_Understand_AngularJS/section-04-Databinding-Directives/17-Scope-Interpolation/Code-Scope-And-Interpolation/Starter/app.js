4aa9017bc59c6786b058415e3fc1ad5f314ff3d2
Siddharth Mishra <siddharth.mishra@motorolasolutions.com>
2016-10-18 13:53:46 -0600

    Injecting name and timeout stuff.

--- Learn_and_Understand_AngularJS/section-04-Databinding-Directives/17-Scope-Interpolation/Code-Scope-And-Interpolation/Starter/app.js
+++ Learn_and_Understand_AngularJS/section-04-Databinding-Directives/17-Scope-Interpolation/Code-Scope-And-Interpolation/Starter/app.js
@@ -1,6 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.name = 'Siddharth';
+    $scope.address = '7067 S Santa Cruz Dr., Apt RS22, Cottonwood Heights, UT-84121';
 
-
+    $timeout(function(){
+        $scope.name = 'Priyanka Pandey';
+    }, 3000);
 }]);
